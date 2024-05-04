906895af279dad4540e020575ac91248d8f358dc
Mohammad Mehdi Jamalpour <mohammadmehdi.jamalpour28@gmail.com>
2024-05-04 21:50:01 +0330

    filter based on search input method added + restyle transaction with grid instead of flex

--- Task-2-SRC-Start/src/js/index.js
+++ Task-2-SRC-Start/src/js/index.js
@@ -1,10 +1,18 @@
 const searchInput = document.querySelector("#search");
 const transactions = document.querySelector(".transaction-list__body");
-const btn = document.querySelectorAll(".btn");
 let allTransactionsData = [];
 const filters = {
   searchItems: "",
 };
+const uploadBtn = document.getElementById("upload-btn");
+const transactionList = document.querySelector(".transactions-list-container");
+const divBtn = document.querySelector(".upload");
+const searchLabel = document.querySelector(".search-label");
+uploadBtn.addEventListener("click", function () {
+  divBtn.classList.add("hidden");
+  transactionList.classList.remove("hidden");
+  searchLabel.classList.remove("hidden");
+});
 
 document.addEventListener("DOMContentLoaded", () => {
   axios
@@ -19,18 +27,15 @@ document.addEventListener("DOMContentLoaded", () => {
 });
 
 function renderTransactions(_transactions, _filters) {
-  const filteredTransaction =_transactions.filter(p => {
-      return p.refId;
-
-  })
+  const filteredTransaction = _transactions.filter((p) => {
+    // console.log(filters);
+    return new String(p.refId).includes(_filters.searchItems.trim());
+  });
 
   transactions.innerHTML = ``;
 
-
   // render to DOM :
-  _transactions.forEach((item, index) => {
-
-    
+  filteredTransaction.forEach((item, index) => {
     let options = { year: "numeric", month: "numeric", day: "numeric" };
     let timeOptions = { hour: "2-digit", minute: "2-digit" };
     const localDate = new Date(item.date).toLocaleDateString("fa-IR", options);
@@ -38,8 +43,8 @@ function renderTransactions(_transactions, _filters) {
       "fa-IR",
       timeOptions
     );
-  const statusColor = checkColor(item)
-  console.log();
+    const statusColor = checkColor(item);
+    console.log();
     // create
     //add content
     // append to products
@@ -58,9 +63,13 @@ function renderTransactions(_transactions, _filters) {
 }
 
 // Status Color style Function
-const checkColor = (item) => item.type ===  "افزایش اعتبار" ? "transaction__success" : "transaction__fail";
-// searchInput.addEventListener('input',(e)=>{
-//   // console.log(e.target.value);
-//   filters.searchItems = e.target.value ;
-//   // renderProducts(allTransactionsData,filters)
-// })
+
+const checkColor = (item) =>
+  item.type === "افزایش اعتبار" ? "transaction__success" : "transaction__fail";
+
+
+
+searchInput.addEventListener("input", (e) => {
+  filters.searchItems = e.target.value;
+  renderTransactions(allTransactionsData, filters);
+});
